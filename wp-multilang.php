1d79464cd0ccafa2bd909fbc20b4a9a4210edf6a
Valentyn Riaboshtan <valexar12@gmail.com>
2017-05-29 19:31:14 +0300

    update version

--- wp-multilang.php
+++ wp-multilang.php
@@ -8,7 +8,7 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:     wpm
  * Domain Path:     /languages
- * Version:         1.1.1
+ * Version:         1.1.2
  *
  * @package  WPM
  * @category Core
