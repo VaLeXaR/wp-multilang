9df9be1ad01afea137c2fe639ab75899cfb7ed4f
Valentyn Riaboshtan <valexar12@gmail.com>
2017-10-23 10:10:41 +0300

    fix uninstalling

--- uninstall.php
+++ uninstall.php
@@ -15,7 +15,7 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 
 global $wpdb;
 
-require_once __DIR__ . '/lib/autoload.php';
+require_once __DIR__ . '/core/autoload.php';
 
 $uninstall_translations = get_option( 'wpm_uninstall_translations', '' );
 
