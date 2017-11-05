a9a83e831eb4e813e4e9f814b751927009ee1077
Valentyn Riaboshtan <valexar12@gmail.com>
2017-11-05 21:35:23 +0200

    add settings for redirect and prefix

--- includes/wpm-translation-functions.php
+++ includes/wpm-translation-functions.php
@@ -75,11 +75,13 @@ function wpm_translate_url( $url, $language = '' ) {
 		}
 	}
 
+	$new_url = '';
+
 	if ( $new_path ) {
-		$url = str_replace( $host . $path, $host . $new_path, $url );
+		$new_url = str_replace( $host . $path, $host . $new_path, $url );
 	}
 
-	return apply_filters( 'wpm_translate_url', $url );
+	return apply_filters( 'wpm_translate_url', $new_url, $url, $language );
 }
 
 /**
