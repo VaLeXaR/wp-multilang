53f297a40b4703238feed8e1a17cda36950c8025
Valentyn Riaboshtan <valexar12@gmail.com>
2017-11-23 09:09:52 +0200

    fix translating html widget

--- includes/wpm-config-functions.php
+++ includes/wpm-config-functions.php
@@ -82,8 +82,9 @@ function wpm_get_widget_config( $widget ) {
 	if ( ! is_null( $widget_config ) ) {
 
 		$default_fields = array(
-			'title' => array(),
-			'text'  => array(),
+			'title'   => array(),
+			'text'    => array(),
+			'content' => array(),
 		);
 
 		$widget_config = wpm_array_merge_recursive( $default_fields, $widget_config );
