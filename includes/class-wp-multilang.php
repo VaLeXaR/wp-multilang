ea74bf075478eeaa2c9a0e246d8e1005c38d8491
Valentyn Riaboshtan <valexar12@gmail.com>
2017-12-09 11:35:35 +0200

    fix panel siteorigin integration

--- includes/class-wp-multilang.php
+++ includes/class-wp-multilang.php
@@ -21,7 +21,7 @@ final class WP_Multilang {
 	 *
 	 * @var string
 	 */
-	public $version = '2.1.6';
+	public $version = '2.1.7';
 
 	/**
 	 * The single instance of the class.
--- wp-multilang.php
+++ wp-multilang.php
@@ -8,7 +8,7 @@
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:     wp-multilang
  * Domain Path:     /languages
- * Version:         2.1.6
+ * Version:         2.1.7
  * Copyright:       © 2017 Valentyn Riaboshtan
  *
  * @package  WPM
