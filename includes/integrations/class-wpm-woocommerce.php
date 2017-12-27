58f4db361369f9c4509b2734e532395fac7ad355
Valentyn Riaboshtan <valexar12@gmail.com>
2017-12-27 15:39:56 +0200

    fix product duplication in WC

--- includes/integrations/class-wpm-woocommerce.php
+++ includes/integrations/class-wpm-woocommerce.php
@@ -26,6 +26,7 @@ class WPM_WooCommerce {
 		add_filter( 'woocommerce_product_get_name', 'wpm_translate_string' );
 		add_filter( 'woocommerce_product_get_description', 'wpm_translate_string' );
 		add_filter( 'woocommerce_product_get_short_description', 'wpm_translate_string' );
+		add_filter( 'woocommerce_product_title', 'wpm_translate_string' );
 		add_filter( 'woocommerce_shortcode_products_query', array( $this, 'remove_filter' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_js_frontend' ) );
 		add_filter( 'woocommerce_cart_shipping_method_full_label', 'wpm_translate_string' );
@@ -304,5 +305,6 @@ class WPM_WooCommerce {
 		remove_filter( 'woocommerce_product_get_name', 'wpm_translate_string' );
 		remove_filter( 'woocommerce_product_get_description', 'wpm_translate_string' );
 		remove_filter( 'woocommerce_product_get_short_description', 'wpm_translate_string' );
+		remove_filter( 'woocommerce_product_title', 'wpm_translate_string' );
 	}
 }
