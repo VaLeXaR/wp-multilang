af237b6f7b7213629fcdf6aeb941f8cab4b2f0a8
Valentyn Riaboshtan <valexar12@gmail.com>
2018-03-23 15:28:01 +0200

    fix http to https redirect error

--- includes/class-wpm-setup.php
+++ includes/class-wpm-setup.php
@@ -341,7 +341,7 @@ class WPM_Setup {
 		}
 
 		if ( $url ) {
-			$this->original_request_uri = str_replace( $this->get_original_home_url(), '', $url );
+			$this->original_request_uri = str_replace( set_url_scheme( $this->get_original_home_url() ), '', $url );
 
 			if ( $url_lang = $this->get_lang_from_url() ) {
 				$user_language = $url_lang;
