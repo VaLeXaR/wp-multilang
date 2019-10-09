42c4e1262bfb5824bc7015f5faa55ee6e3a7eec8
Yunus Gaziev <yunusga@yandex.ru>
2019-10-10 01:25:32 +0500

    Restore the_excerpt filter

--- includes/class-wpm-posts.php
+++ includes/class-wpm-posts.php
@@ -42,6 +42,7 @@ class WPM_Posts extends WPM_Object {
 		add_filter( 'the_post', 'wpm_translate_post', 5 );
 		add_filter( 'the_title', 'wpm_translate_string', 5 );
 		add_filter( 'the_content', 'wpm_translate_string', 5 );
+		add_filter( 'the_excerpt', 'wpm_translate_string', 5 );
 		add_filter( 'get_the_excerpt', 'wpm_translate_string', 5 );
 		add_filter( 'the_editor_content', 'wpm_translate_string', 5 );
 		add_action( 'parse_query', array( $this, 'filter_posts_by_language' ) );
