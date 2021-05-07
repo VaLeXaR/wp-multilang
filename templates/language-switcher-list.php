fbcc11da9fdd344574b033f303436560beac1cd2
Damir Calusic <damir@damircalusic.com>
2021-05-08 00:13:29 +0200

    Fix so flag width and height outputs properly on wpml langauge listing regarding SEO

--- templates/language-switcher-list.php
+++ templates/language-switcher-list.php
@@ -1,6 +1,10 @@
 <?php /** @var $show string */ ?>
 <ul class="wpm-language-switcher switcher-<?php esc_attr_e( $type ); ?>">
-	<?php foreach ( $languages as $code => $language ) { ?>
+	<?php 
+	foreach ( $languages as $code => $language ) { 
+		$src = esc_url( wpm_get_flag_url( $language['flag'] ) );
+		$size = getimagesize( $src );
+		?>
 		<li class="item-language-<?php echo esc_attr( $code ); ?><?php if ( $code === $lang ) { ?> active<?php } ?>">
 			<?php if ( wpm_get_language() == $code ) { ?>
 				<span data-lang="<?php echo esc_attr( $code ); ?>">
@@ -8,7 +12,7 @@
 				<a href="<?php echo esc_url( wpm_translate_current_url( $code ) ); ?>" data-lang="<?php echo esc_attr( $code ); ?>">
 			<?php } ?>
 				<?php if ( ( ( 'flag' === $show ) || ( 'both' === $show ) ) && ( $language['flag'] ) ) { ?>
-					<img src="<?php echo esc_url( wpm_get_flag_url( $language['flag'] ) ); ?>" alt="<?php echo esc_attr( $language['name'] ); ?>">
+					<img src="<?php echo $src; ?>" <?php echo ( is_array($size) ) ? $size[3] : ''; ?> alt="<?php echo esc_attr( $language['name'] ); ?>">
 				<?php } ?>
 				<?php if ( ( 'name' === $show ) || ( 'both' === $show ) ) { ?>
 					<span><?php echo esc_html( $language['name'] ); ?></span>
