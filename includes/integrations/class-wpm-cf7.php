7d00968b1dff43a0c6adb719700dd53bfcfaeac5
Damir Calusic <damir@damircalusic.com>
2021-05-08 00:06:41 +0200

    Fix translation of the "title" attribute in the shortcode of Contact Form 7

--- includes/integrations/class-wpm-cf7.php
+++ includes/integrations/class-wpm-cf7.php
@@ -22,8 +22,9 @@ class WPM_CF7 {
 	 */
 	public function __construct() {
 		add_filter( 'wpcf7_special_mail_tags', array( $this, 'add_language_tag' ), 10, 2 );
-		add_filter( 'wpcf7_form_hidden_fields', array( $this, 'add_lang_field' ) );
 		add_filter( 'wpcf7_special_mail_tags', array( $this, 'translate_post_title' ), 11, 2 );
+		add_filter( 'wpcf7_form_hidden_fields', array( $this, 'add_lang_field' ) );
+		add_action( 'wpcf7_contact_form', array( $this, 'edit_form_translate_shortcode_title_attr' ), 10, 1 );
 	}
 
 	public function add_language_tag( $output, $name ) {
@@ -34,20 +35,6 @@ class WPM_CF7 {
 		}
 
 		return $output;
-	}
-
-
-	/**
-	 * Add current user language hidden field
-	 *
-	 * @param $fields
-	 *
-	 * @return mixed
-	 */
-	public function add_lang_field( $fields ) {
-		$fields['lang'] = wpm_get_language();
-
-		return $fields;
 	}
 
 	/**
@@ -64,5 +51,27 @@ class WPM_CF7 {
 		}
 
 		return $output;
+	}
+
+	/**
+	 * Add current user language hidden field
+	 *
+	 * @param $fields
+	 *
+	 * @return mixed
+	 */
+	public function add_lang_field( $fields ) {
+		$fields['lang'] = wpm_get_language();
+
+		return $fields;
+	}
+
+	/**
+	 * Fix translation of the "title" attribute in the shortcode for copying, on the form edit page
+	 *
+	 * @param $wpcf7 current Contacts Form 7 instance
+	 */
+	public function edit_form_translate_shortcode_title_attr( $wpcf7 ) {
+		$wpcf7->set_title( wpm_translate_string( $wpcf7->title() ) );
 	}
 }
