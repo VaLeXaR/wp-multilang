4b45619e217f080f13e1d78e3bd40785bb5f6d3d
Damir Calusic <damir@damircalusic.com>
2021-08-06 12:23:32 +0200

    Changed version

--- wp-multilang.php
+++ wp-multilang.php
@@ -9,7 +9,7 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       wp-multilang
  * Domain Path:       /languages
- * Version:           2.4.2
+ * Version:           2.4.3
  * Copyright:         © 2017-2019 Valentyn Riaboshtan
  *
  * @package  WPM
